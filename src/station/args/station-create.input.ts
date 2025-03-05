8774e1c232d606c502622df4f48ae9f0cb010c4a
Jakkrit Onsomkrit <jakkrit.o@gli.co.th>
2025-03-06 01:25:00 +0700

    add packages and validation

--- src/station/args/station-create.input.ts
+++ src/station/args/station-create.input.ts
@@ -1,4 +1,5 @@
 import { InputType, Field } from '@nestjs/graphql';
+// import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 @InputType()
 export class StationCreateInput {
@@ -9,6 +10,9 @@ export class StationCreateInput {
   name: string;
 
   @Field({ nullable: true })
+  // @IsNotEmpty({ message: 'Name is required' })
+  // @IsString()
+  // @Length(3, 50, { message: 'Name must be between 3 and 50 characters' })
   code: string;
 
   @Field()
