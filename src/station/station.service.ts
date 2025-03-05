9aca8bc8f2c70987c16f653b33e87c96d1a0cfb6
Jakkrit Onsomkrit <jakkrit.o@gli.co.th>
2025-03-06 00:18:55 +0700

    add create station

--- src/station/station.service.ts
+++ src/station/station.service.ts
@@ -11,7 +11,7 @@ export class StationService {
   ) {}
 
   create(stationCreateInput: StationCreateInput) {
-    return stationCreateInput;
+    return this.stations.create(stationCreateInput);
   }
 
   findAll(): Promise<Station[]> {
