ceef5f50231f4516ec2f7c347d8af1c65f13f175
Jakkrit Onsomkrit <jakkrit.o@gli.co.th>
2025-03-10 23:20:16 +0700

    add check path /version

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { Utility, Services } from 'rpro-utility';
+
+import * as packageJson from '../package.json';
+
+const { VersionService } = Services;
+const { KafkaHealthCheck, Redis } = Utility;
+const redisClient = Redis.getInstance().getClient();
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getVersion(): Promise<any> {
+    async function getKafkaConnection() {
+      try {
+        if (typeof KafkaHealthCheck !== 'function') {
+          throw new Error(
+            'KafkaHealthCheck is not available in the sharing module',
+          );
+        }
+        return (await KafkaHealthCheck()) ? 'connected' : 'disconnected';
+      } catch (e: any) {
+        console.log('Error getKafkaConnection() :', e);
+        return 'disconnected';
+      }
+    }
+
+    const sharingModules = await VersionService.getVersion();
+    const formattedServiceName = sharingModules.service.replace(/-/g, '_');
+    const redisStatus = await redisClient.set('redisConnection', 'ok');
+    const redisConnection = redisStatus === 'OK' ? 'connected' : 'disconnected';
+
+    const response = {
+      service: packageJson.name || 'unknown',
+      version: `v${packageJson.version || 'unknown'}`,
+      [formattedServiceName]: `${sharingModules.version}`,
+      redis_connection: redisConnection,
+      kafka_connection: await getKafkaConnection(),
+    };
+
+    return response;
   }
 }
